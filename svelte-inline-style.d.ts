8bc29048c1cc4931ab773e84ac913dcd1cfa9d06
Andreas Rozek <A.Rozek@gmx.de>
2021-06-16 18:17:05 +0200

    change folder structure

--- svelte-inline-style.d.ts
+++
@@ -1,8 +0,0 @@
-/**** use:style={styleset} - with camel-cased style properties ****/
-declare type StyleSet = {
-    [Name: string]: string | number;
-};
-declare function style(Element: HTMLElement, initialStyleSet: StyleSet): {
-    update: (newStyleSet: StyleSet) => void;
-};
-export default style;
--- svelte-inline-style.esm.js
+++
@@ -1,2 +0,0 @@
-function t(t,n){function r(n){for(var r in n)n.hasOwnProperty(r)&&(t.style[r]=n[r])}return r(n),{update:r}}export default t;
-//# sourceMappingURL=svelte-inline-style.esm.js.map
