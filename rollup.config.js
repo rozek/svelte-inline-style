1fd09f0d21a6da893ce7cb843bb7b34796b1dae1
Andreas Rozek <A.Rozek@gmx.de>
2021-07-05 09:01:28 +0200

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
-// see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
+// see https://github.com/rozek/build-configuration-study
 
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/svelte-inline-style.ts',
@@ -10,5 +9,7 @@ export default {
     format:   'esm',
     sourcemap:true
   },
-  plugins: [typescript(), terser({ format:{ comments:false, safari10:true } })],
+  plugins: [
+    typescript(),
+  ],
 };
